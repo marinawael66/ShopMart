bcb51f53db5d50ddb05b473b78ec53af14c00867
marinawael66 <marinawael666@gmail.com>
2025-10-06 06:59:06 +0300

    change cookie name

--- src/Helpers/getUserToken.ts
+++ src/Helpers/getUserToken.ts
@@ -4,7 +4,8 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
- const x =(await cookies()).get('next-auth.session-token')?.value;
+    const tokenSession = (process.env.NODE_ENV==="production"?"__Secure-next-auth.session-token" : "next-auth.session-token");
+ const x =(await cookies()).get(tokenSession)?.value;
     const accessToken= await decode({token:x,secret:process.env.NEXTAUTH_SECRET!})
     return accessToken?.token;
 }
